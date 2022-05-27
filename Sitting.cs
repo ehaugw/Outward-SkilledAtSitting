50296d6c15572dc818d2093d5988293a5e74276d
Eivind <ehaugw@gmail.com>
2022-05-27 11:01:39 +0200

    initial commit

---
+++ Sitting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkilledAtSitting
+{
+    using SideLoader;
+    using UnityEngine;
+
+    class Sitting : Effect
+    {
+        public const float TICK_RATE = 1;
+        protected override void ActivateLocally(Character _affectedCharacter, object[] _infos)
+        {
+            if ((((_affectedCharacter?.Animator?.velocity != null) && (_affectedCharacter.Animator.velocity.sqrMagnitude > 0.1))|| ((_affectedCharacter?.AnimMoveSqMagnitude ?? 0) > 0.1 && this.m_parentStatusEffect.Age > 0.5)) && this.m_parentStatusEffect.Age > 1)
+            {
+                _affectedCharacter.StatusEffectMngr.CleanseStatusEffect(SkilledAtSitting.SITTING_EFFECT_NAME);
+            }
+
+            float ticksPerGameHour = 3600f / 24f / TICK_RATE;
+            float effectiveness = 0.75f;
+
+            _affectedCharacter.Stats.AffectHealth(35 / ticksPerGameHour * effectiveness);
+            _affectedCharacter.Stats.RestoreBurntHealth(0.05f / ticksPerGameHour * effectiveness, true);
+            _affectedCharacter.Stats.RestoreBurntStamina(20 / ticksPerGameHour * effectiveness, false);
+        }
+    }
+}
---
+++ SkilledAtSitting.cs
@@ -0,0 +1,96 @@
+﻿namespace SkilledAtSitting
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using SideLoader;
+    using HarmonyLib;
+    using BepInEx;
+    using InstanceIDs;
+    using TinyHelper;
+
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class SkilledAtSitting : BaseUnityPlugin
+    {
+        public const string GUID = "com.ehaugw.skilledatsitting";
+        public const string VERSION = "4.2.0";
+        public const string NAME = "Skilled at Sitting";
+
+        public Skill sitInstance;
+
+        internal void Awake()
+        {
+            SL.OnPacksLoaded += OnPackLoaded;
+            SL.OnSceneLoaded += OnSceneLoaded;
+
+            //var harmony = new Harmony(GUID);
+            //harmony.PatchAll();
+        }
+
+        private void OnSceneLoaded()
+        {
+            foreach (Character chr in  CharacterManager.Instance.Characters.Values)
+            {
+                if (!chr.IsAI && chr.Inventory != null && !chr.Inventory.LearnedSkill(sitInstance))
+                {
+                    chr.Inventory.ReceiveSkillReward(sitInstance.ItemID);
+                } 
+            }
+        }
+        private void OnPackLoaded()
+        {
+            MakeSittingPrefab();
+            sitInstance = Sit();
+        }
+
+        private Skill Sit()
+        {
+            var myitem = new SL_Skill()
+            {
+                Name = "Sit",
+                EffectBehaviour = EditBehaviours.Destroy,
+                Target_ItemID = IDs.pushKickID,
+                New_ItemID = IDs.sitID,
+                SLPackName = "SkilledAtSitting",
+                SubfolderName = "Sit",
+                Description = "Sit down to regain health, burnt health and burnt stamina.",
+                
+                CastType = Character.SpellCastType.Sit,
+                CastModifier = Character.SpellCastModifier.Immobilized,
+                CastLocomotionEnabled = true,
+                MobileCastMovementMult = -1f,
+
+                EffectTransforms = new SL_EffectTransform[] {
+                    new SL_EffectTransform() {
+                        TransformName = "Effects",
+                        Effects = new SL_Effect[] {
+                            new SL_AddStatusEffect() {StatusEffect = SITTING_EFFECT_NAME, ChanceToContract=100, Delay = 2},
+                        }
+                    }
+                },
+                Cooldown = 1,
+                StaminaCost = 0,
+                HealthCost = 0,
+                ManaCost = 0,
+            };
+            myitem.ApplyTemplate();
+            Skill skill = ResourcesPrefabManager.Instance.GetItemPrefab(myitem.New_ItemID) as Skill;
+            skill.IgnoreLearnNotification = true;
+            return skill;
+        }
+
+        public static string SITTING_EFFECT_NAME = "Sitting";
+
+        private void MakeSittingPrefab()
+        {
+            var statusEffect = TinyEffectManager.MakeStatusEffectPrefab(SITTING_EFFECT_NAME, SITTING_EFFECT_NAME, "Regain health, burnt health and stamina while sitting.", -1, Sitting.TICK_RATE, StatusEffectFamily.StackBehaviors.Override, "Bandage", false, modGUID: GUID);
+
+            var effectSignature = statusEffect.StatusEffectSignature;
+            var effectComponent = TinyGameObjectManager.MakeFreshObject("Effects", true, true, effectSignature.transform).AddComponent<Sitting>();
+            effectComponent.UseOnce = false;
+            effectSignature.Effects = new List<Effect>() { effectComponent };
+
+            statusEffect.IsHidden = false;
+            statusEffect.DisplayInHud = true;
+        }
+    }
+}
